0b883459a58973fc4f3814c181d1633051653c5f
HamNP <ntwpjtr@gmail.com>
2025-06-24 16:44:04 +0700

    second commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import app from './app.js'
+import prisma from './config/prisma.config.js'
 
 dotenv.config()
 // console.log(process.env.PORT)
@@ -10,3 +11,22 @@ const PORT = process.env.PORT || 8000
 
 app.listen(PORT,()=>console.log('Server on :',PORT))
 
+// prisma.user.count().then(rs => console.log(rs))
+// prisma.user.count().then(console.log)
+
+prisma.$queryRaw`Select * from user`.then(console.log())
+
+// *** เพิ่ม code ไม่ใช่ copy ทับ ***
+import shutdown from './utils/shutdown.util.js'
+
+// Listen for termination signals
+process.on('SIGINT', () => {
+  prisma.$disconnect().then(()=>{
+    console.log('\nprisma shutdown')
+    process.exit(0)
+  })
+});   // Ctrl+C
+process.on('SIGTERM', () => shutdown('SIGTERM')); // kill command or Docker stop
+// Catch unhandled errors
+process.on("uncaughtException", ()=>  shutdown('uncaughtException'))
+process.on("unhandledRejection", ()=> shutdown('unhandledRejection'))
